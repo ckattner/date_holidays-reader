0c6f9d12ce5805fbe62d46cb159a48d17622daa0
Ryan Gerry <rgerry@bluemarblepayroll.com>
2019-02-05 11:25:52 -0600

    Adding copyright to bin files.

--- bin/date-holidays-version.js
+++ bin/date-holidays-version.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/*
+ Copyright(c) 2019 - present, Blue Marble Payroll, LLC
+
+ This source code is licensed under the MIT license found in the
+ LICENSE file in the root directory of this source tree.
+*/
+
 'use strict'
 
 const path = require('path');
